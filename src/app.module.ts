dc1087971739243caf5c9390501bc7d82737e227
Yevhenii Solovei <146028527+TheDidEva@users.noreply.github.com>
2023-12-10 20:15:25 +0200

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
+import { User } from "./users/user.model";
+
+
+//в Nest весь синтаксиб був збудований навколо декораторів
+//декоратор - додає класу або функції новий функціонал
+@Module({
+    controllers: [],//контролерів може бути багато
+    providers: [],//провадером можу бути буль який реюзабельний елемент, це можуть бути сервіси з логікою імлемінтації атернів, стратегій
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: String(process.env.POSTGRES_PASSWORD),
+            database: process.env.POSTGRES_DB,
+            models: [User],
+            autoLoadModels: true,
+        }),
+        UsersModule,
+    ],
+})
+export class AppModule {
+
+}
